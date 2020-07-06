edf98bea8a3165650e83deb367c2aaf7ada93950
Lucas Godoi <godoi.lucas@hotmail.com>
2020-07-06 16:25:57 -0300

    CRUD com Sequelize

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -10,6 +10,50 @@ class PessoaController {
         }
        
     }
+
+    static async pegaUmaPessoa(req, res){
+        const { id } = req.params;
+        try {
+            const umaPessoa = await database.Pessoas.findOne({ where: { id: Number(id) }});
+            return res.status(200).json(umaPessoa);
+        } catch (error) {
+            return res.status(500).json(erro.message);
+        }
+    }
+
+    static async criaPessoa(req, res){
+        const novaPessoa = req.body;
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+            return res.status(201).json(novaPessoaCriada);
+        } catch (error) {
+            return res.status(500).json(erro.message);
+        }
+    }
+
+    static async atualizaPessoa(req, res){
+        const { id } = req.params;
+        const novasInfos = req.body;
+
+        try {
+            await database.Pessoas.update(novasInfos, { where: { id: Number(id) }});
+            const pessoaAtualizada = await database.Pessoas.findOne({ where: { id: Number(id) }});
+            return res.status(200).json(pessoaAtualizada);
+        } catch (error) {
+            return res.status(500).json(erro.message);
+        }
+    }
+
+    static async apagaPessoa(req, res){
+        const { id } = req.params;
+
+        try {
+            await database.Pessoas.destroy({ where: { id: Number(id) }});
+            return res.status(200).json({ message: `id ${id} deletado` });
+        } catch (error) {
+            return res.status(500).json(erro.message);
+        }
+    }
 }
 
 module.exports = PessoaController;
